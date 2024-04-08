4849b772ddf3178842fa4f5cca02f2b3044a58c6
Joao_Maia <joaoagrivmf@gmail.com>
2024-04-08 11:27:24 +0000

    initial

--- lib/login_util.dart
+++ lib/login_util.dart
@@ -1,69 +1,75 @@
-Future<void> _login(BuildContext context) async {
-  String username = ''; // Get username from text field
-  String password = ''; // Get password from text field
+import 'package:syntzapp/api_util.dart';
+import 'package:flutter/material.dart';
+import 'package:syntzapp/config_util.dart';
 
-  bool loginSuccess = await simulateAPICall(username, password);
-  if (loginSuccess) {
-    // If login is successful, set token and expiration date
-    Config.setTokenExpiration(DateTime.now().millisecondsSinceEpoch +
-        3600 * 1000); // Example: 1 hour from now
-  } else {
-    // If login fails, show error message and return
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Error'),
-          content: Text('Invalid username or password.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-    return;
+class LoginUtil {
+  Future<void> login(BuildContext context) async {
+    String username = ''; // Get username from text field
+    String password = ''; // Get password from text field
+
+    bool loginSuccess = await ApiUtil().callAPI(username, password);
+    if (loginSuccess) {
+      // If login is successful, set token and expiration date
+      ConfigUtil.setTokenExpiration(DateTime.now().millisecondsSinceEpoch +
+          3600 * 1000); // Example: 1 hour from now
+    } else {
+      // If login fails, show error message and return
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Invalid username or password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+
+    // Check if the token expiration is still valid after API call
+    // Navigation logic...
   }
 
-  // Check if the token expiration is still valid after API call
-  // Navigation logic...
-}
+  Future<void> login2(BuildContext context) async {
+    String username = ''; // Get username from text field
+    String password = ''; // Get password from text field
 
-Future<void> _login(BuildContext context) async {
-  String username = ''; // Get username from text field
-  String password = ''; // Get password from text field
+    bool loginSuccess = await ApiUtil().callAPI(username, password);
+    if (loginSuccess) {
+      // If login is successful, set token and expiration date
+      ConfigUtil.setTokenExpiration(DateTime.now().millisecondsSinceEpoch +
+          3600 * 1000); // Example: 1 hour from now
+    } else {
+      // If login fails, show error message and return
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('Invalid username or password.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
 
-  bool loginSuccess = await simulateAPICall(username, password);
-  if (loginSuccess) {
-    // If login is successful, set token and expiration date
-    Config.setTokenExpiration(DateTime.now().millisecondsSinceEpoch +
-        3600 * 1000); // Example: 1 hour from now
-  } else {
-    // If login fails, show error message and return
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Error'),
-          content: Text('Invalid username or password.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-    return;
+    // Check if the token expiration is still valid after API call
+    // Navigation logic...
   }
-
-  // Check if the token expiration is still valid after API call
-  // Navigation logic...
 }
